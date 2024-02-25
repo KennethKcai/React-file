08bfeb285090e78e786dcaefc2cbea9fdb8c0d19
KennethKcai <97841755+KennethKcai@users.noreply.github.com>
2024-02-24 18:56:25 -0800

    inis

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import { useEffect } from 'react'
+import './App.css'
 
 //4c15de18
 
 const API_URL = 'http://www.omdbapi.com?apikey=4c15de18'
 
+
 const App = () => {
 
-  const serchMovies = async (title) => {
+  const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`)
     const data = await response.json()
     console.log(data)
   }
 
   useEffect(() => {
-    serchMovies('avengers')
+    searchMovies('avengers')
   }, [])
   return (
-    <div>App</div>
+    <h1>
+      Hello World
+    </h1>
   )
 }
 
