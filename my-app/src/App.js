b92157b86bbdd9696891786ac2be40306617db75
KennethKcai <97841755+KennethKcai@users.noreply.github.com>
2024-03-14 00:06:39 -0700

    card

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,19 @@ import React from 'react'
 import { useEffect } from 'react'
 import './App.css'
 import searchIcon from './search.svg'
+import MovieCard from './MovieCard'
 
 //4c15de18
 
 const API_URL = 'http://www.omdbapi.com?apikey=4c15de18'
 
+const movie1 = {
+  "Title": "Ultimate Avengers II",
+  "Year": "2006",
+  "imdbID": "tt0803093",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BZjI3MTI5ZTYtZmNmNy00OGZmLTlhNWMtNjZiYmYzNDhlOGRkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+}
 
 const App = () => {
 
@@ -31,6 +39,10 @@ const App = () => {
           alt='Search Icon'
           onClick={() => {}}
         />
+      </div>
+
+      <div className='container'>
+        <MovieCard movie1={movie1} />
       </div>
 
     </div>
